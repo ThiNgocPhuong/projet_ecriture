819b7566caf29ca803be2bc847b97bf794132d8a
ThiNgocPhuong <105435187+ThiNgocPhuong@users.noreply.github.com>
2022-12-30 19:00:12 +0100

    Add files via upload

--- base.sql
+++ base.sql
@@ -1,4 +1,4 @@
-create database Histoire;
+create database Projet_ecriture;
 
 create table Idee(
     id INTEGER NOT NULL AUTO_INCREMENT,
@@ -9,11 +9,12 @@ create table Idee(
     PRIMARY KEY (id)
 );
 
-create table Idee_Utiliser(
+create table Histoire(
      id_idee INTEGER NOT NULL AUTO_INCREMENT,
      titre_histoire VARCHAR(60),
      date_fin_ecriture DATE,
      resume VARCHAR(8000),
+     vote INTEGER,
      PRIMARY KEY (id_idee),
      FOREIGN KEY (id_idee) REFERENCES Idee(id) 
 );
@@ -25,5 +26,19 @@ create table Corriger(
     FOREIGN KEY (id_idee) REFERENCES Idee(id) 
 );
 
+create table User(
+    email VARCHAR(800) unique NOT NULL,
+    mdp VARCHAR(800) unique NOT NULL
+)
+
 CREATE USER Phuong IDENTIFIED BY "Ph@ntomhive";
 GRANT ALL PRIVILEGES ON *.* TO 'Phuong'; 
+
+CREATE USER Visiteur IDENTIFIED BY "*O*visiteur";
+GRANT Select ON Projet_ecriture.Idee TO Visiteur;
+GRANT Select ON Projet_ecriture.Histoire TO Visiteur;
+
+CREATE USER Auteur IDENTIFIED BY " @uteur96";
+GRANT  insert, update, delete ON Projet_ecriture.Idee TO Auteur;
+GRANT  insert, update, delete ON Projet_ecriture.Histoire TO Auteur;
+GRANT update ON Projet_ecriture.User TO Auteur;
